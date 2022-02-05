9ef1edc0483d80e0bcf57bc0708d6a38d8986c24
bdomingu <beatrizdominguez14@gmail.com>
2022-02-04 22:26:21 -0700

    Changed print statements

--- Agent.java
+++ Agent.java
@@ -5,8 +5,8 @@ class Agent extends Client {
     public void BuyBTC() {
         System.out.println("This is the buy option");
         System.out.println("Purchases $100 of BTC:" + ' ' + values.updateBTCBalance(100, true));
-        System.out.println("Purchases $860 of BTC:" + ' ' + values.updateETHBalance(860, true));
-        System.out.println("Purchases $80 of BTC:" + ' ' + values.updateBNBBalance(80, true));
+        System.out.println("Purchases $860 of ETH:" + ' ' + values.updateETHBalance(860, true));
+        System.out.println("Purchases $80 of BNB:" + ' ' + values.updateBNBBalance(80, true));
         System.out.println("Subtracts $150 off the USD Balance:" + values.updateUSDBalance(150, "buy"));
 
     }
@@ -14,7 +14,7 @@ class Agent extends Client {
     public void SellBTC() {
         System.out.println("This is the sell option");
         System.out.println("Sells 1 bitcoin:" + ' ' + values.updateBTCBalance(1, false));
-        System.out.println("Sells .2 bitcoin:" + ' ' + values.updateETHBalance(.2, false));
+        System.out.println("Sells .2 Ethereum:" + ' ' + values.updateETHBalance(.2, false));
         System.out.println("Sells .35 BNB:" + ' ' + values.updateBNBBalance(.35, false));
         System.out.println(
                 "Sells .11 BTC and adds it to the USD Wallet:" + ' ' + values.updateUSDBalance(.11, "sellBTC"));
