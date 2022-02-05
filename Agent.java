3680059761f73561de502908af508a81d7ac6bf7
bdomingu <beatrizdominguez14@gmail.com>
2022-02-04 22:01:28 -0700

    Agent updated

--- Agent.java
+++ Agent.java
@@ -2,25 +2,26 @@ class Agent extends Client {
 
     Client values = new Client();
 
-    // remove the void and have it return a boolean
-    // create an instance of client so the variables can be accessed.
     public void BuyBTC() {
         System.out.println("This is the buy option");
-        System.out.println(values.updateBTCBalance(100, true));
-        System.out.println(values.updateETHBalance(860, true));
-        System.out.println(values.updateBNBBalance(80, true));
-        System.out.println(values.updateUSDBalance(150, "buy"));
+        System.out.println("Purchases $100 of BTC:" + ' ' + values.updateBTCBalance(100, true));
+        System.out.println("Purchases $860 of BTC:" + ' ' + values.updateETHBalance(860, true));
+        System.out.println("Purchases $80 of BTC:" + ' ' + values.updateBNBBalance(80, true));
+        System.out.println("Subtracts $150 off the USD Balance:" + values.updateUSDBalance(150, "buy"));
 
     }
 
     public void SellBTC() {
         System.out.println("This is the sell option");
-        System.out.println(values.updateBTCBalance(1, false));
-        System.out.println(values.updateETHBalance(.2, false));
-        System.out.println(values.updateBNBBalance(.35, false));
-        System.out.println(values.updateUSDBalance(.11, "sellBTC"));
-        System.out.println(values.updateUSDBalance(.5, "sellETH"));
-        System.out.println(values.updateUSDBalance(.02, "sellBNB"));
+        System.out.println("Sells 1 bitcoin:" + ' ' + values.updateBTCBalance(1, false));
+        System.out.println("Sells .2 bitcoin:" + ' ' + values.updateETHBalance(.2, false));
+        System.out.println("Sells .35 BNB:" + ' ' + values.updateBNBBalance(.35, false));
+        System.out.println(
+                "Sells .11 BTC and adds it to the USD Wallet:" + ' ' + values.updateUSDBalance(.11, "sellBTC"));
+        System.out
+                .println("Sells .5 ETH and adds it to the USD Wallet:" + ' ' + values.updateUSDBalance(.5, "sellETH"));
+        System.out.println(
+                "Sells .02 BNB and adds it to the USD Wallet:" + ' ' + values.updateUSDBalance(.02, "sellBNB"));
 
     }
 
